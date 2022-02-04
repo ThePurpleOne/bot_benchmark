9d58d7b81b0e07fac4d9c13251fa480d15967d75
ThePurpleOne <jonas@stirnemann.xyz>
2022-02-04 10:09:14 -0800

    Fixed and optimized

--- aim/aim.py
+++ aim/aim.py
@@ -1,12 +1,16 @@
+import numpy as np
+import cv2 as cv
+from PIL import ImageGrab, Image
 import pyautogui
-from time import sleep
+import time
 import mouse
 
+cible = cv.imread('cible.png')
+cible_cv = cv.cvtColor(np.array(cible), cv.COLOR_BGR2GRAY)
 
 for i in range(31):
-    coord = None
-    while coord is None:
-        coord = pyautogui.locateOnScreen(
-            'cible.png', grayscale=True, confidence=0.6)
-
-    pyautogui.click(coord)
+	img = ImageGrab.grab()
+	img_cv = cv.cvtColor(np.array(img), cv.COLOR_BGR2GRAY)
+	pt = cv.matchTemplate(img_cv, cible_cv, cv.TM_CCOEFF_NORMED)
+	min_val, max_val, min_loc, max_loc= cv.minMaxLoc(pt)
+	pyautogui.click(max_loc)
